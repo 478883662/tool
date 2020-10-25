8ebc3e39b4b8d655e172df78856c48555b85fa0b
478883662 <478883662@qq.com>
2020-10-25 11:41:30 +0800

    记录github地址

--- src/main/java/com/zhangb/tool/ToolApplication.java
+++ src/main/java/com/zhangb/tool/ToolApplication.java
@@ -3,6 +3,10 @@ package com.zhangb.tool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 远端github
+ * https://github.com/478883662/tool.git
+ */
 @SpringBootApplication
 public class ToolApplication {
 
